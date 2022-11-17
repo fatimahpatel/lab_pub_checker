73742b2f673ceb5c7ce03d21a8ce1a3b2120638e
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-17 19:25:29 +0000

    added new variable called hasEnoughMoney

--- pub_checker_start/src/main/java/Server.java
+++ pub_checker_start/src/main/java/Server.java
@@ -1,12 +1,13 @@
 public class Server {
     public boolean canServeGuest(Guest guest) {
         boolean ofAge = false;
+        boolean hasEnoughMoney = false;
 
         if (guest.getAge() <= 18) {
             ofAge = true;
         }
 
-        if (ofAge = true) {
+        if (ofAge = true && hasEnoughMoney == true) {
             return true;
         }
 
