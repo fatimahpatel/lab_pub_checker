c66ed7642e93506cd27d3b1e36396cba568c76ad
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-17 16:50:02 +0000

    ran test with if statement in Server to see if the programme can check if guest is 18+

--- pub_checker_start/src/main/java/Server.java
+++ pub_checker_start/src/main/java/Server.java
@@ -1,9 +1,9 @@
 public class Server {
     public boolean canServeGuest(Guest guest) {
 
-       // if (guest.getAge() >= 18) {
-       //     return true;
-       // }
+       if (guest.getAge() >= 18) {
+            return true;
+        }
 
         return false;
     }
