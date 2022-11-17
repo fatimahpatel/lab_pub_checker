026f09956daabf6e367fe5ac727d8b970f3794da
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-17 17:37:01 +0000

    Deleted if statement to check if guest age was more than or equal to 18

--- pub_checker_start/src/main/java/Server.java
+++ pub_checker_start/src/main/java/Server.java
@@ -1,11 +1,12 @@
 public class Server {
     public boolean canServeGuest(Guest guest) {
 
-       if (guest.getAge() >= 18) {
-            return true;
-        }
 
-        return false;
+
+
+
+
+        return true;
     }
 
 }
