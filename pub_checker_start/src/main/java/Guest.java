ea224e4ce74f8e4ecd0a691529965f8ec1f14f8f
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-17 15:32:51 +0000

    edited Guest constructor to include age and overEighteen

--- pub_checker_start/src/main/java/Guest.java
+++ pub_checker_start/src/main/java/Guest.java
@@ -1,8 +1,12 @@
 public class Guest {
 
     private String name;
+    private int age;
+    private boolean overEighteen;
 
     public Guest(String name){
         this.name = name;
+        this.age = age;
+        this.overEighteen = overEighteen;
     }
 }
